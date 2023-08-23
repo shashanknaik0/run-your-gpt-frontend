9685c72ab4653138322073631a7261dc23a238b8
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-23 10:43:35 +0530

    Add. create chat componant

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Chat from './componants/chat/Chat';
 
 function App() {
   return (
     <div className="App">
-
+      <Chat />
     </div>
   );
 }
---
+++ src/componants/chat/Chat.js
@@ -0,0 +1,7 @@
+const Chat=()=>{
+    return (
+        <h1>hello</h1>
+    );
+}
+
+export default Chat;
